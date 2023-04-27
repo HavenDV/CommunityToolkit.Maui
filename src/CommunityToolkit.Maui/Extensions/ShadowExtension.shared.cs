c1180b33423e4ada382c98706f47134eaae91f16
Konstantin S <havendv@gmail.com>
2023-04-27 17:47:57 +0400

    feat: Added ShadowExtension markup extension.

---
+++ src/CommunityToolkit.Maui/Extensions/ShadowExtension.shared.cs
@@ -0,0 +1,43 @@
+﻿namespace CommunityToolkit.Maui.Extensions;
+
+/// <inheritdoc />
+public class ShadowExtension : IMarkupExtension<Shadow>
+{
+	/// <summary>
+	/// Default value: 10.0f.
+	/// </summary>
+	public float Radius { get; set; } = 10.0f;
+	
+	/// <summary>
+	/// Default value: 1.0f.
+	/// </summary>
+	public float Opacity { get; set; } = 1.0f;
+	
+	/// <summary>
+	/// Default value: Brush.Black.
+	/// </summary>
+	public Brush Brush { get; set; } = Brush.Black;
+	
+	/// <summary>
+	/// Default value: 10, 10.
+	/// </summary>
+	public Point Offset { get; set; } = new(x: 10, y: 10);
+	
+	/// <inheritdoc />
+	public Shadow ProvideValue(IServiceProvider serviceProvider)
+	{
+		return new Shadow
+		{
+			Radius = Radius,
+			Opacity = Opacity,
+			Brush = Brush,
+			Offset = Offset,
+		};
+	}
+
+	/// <inheritdoc />
+	object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+	{
+		return ProvideValue(serviceProvider);
+	}
+}
